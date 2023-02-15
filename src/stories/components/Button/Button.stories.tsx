0e337f9d7ec0fbf57d7bdd5fd7bf4188fb1c370b
Benoît <61755435+Mowdyy@users.noreply.github.com>
2023-02-15 12:07:33 +0100

    Feat/improve button (#8)
    * Add disabled state + improve button display in storybook
    
    * Setup WithIcon button section

--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -12,39 +12,77 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args}>{args.children}</Button>
+  <div className={'btn-stack'}>
+    <Button {...args}>{args.children}</Button>
+  </div>
 );
 
 export const Default = Template.bind({});
 Default.args = { children: 'Button' };
 
 export const All = () => (
-  <div className={'btn-stack'}>
-    <Button color="primary">Filled Primary</Button>
-    <Button color="success">Filled Success</Button>
-    <Button variant="outlined" color="primary">
-      Outlined Primary
-    </Button>
-    <Button variant="outlined" color="success">
-      Outlined Success
-    </Button>
-    <Button variant="void" color="primary">
-      Void Primary
-    </Button>
-    <Button variant="void" color="success">
-      Void Success
-    </Button>
-  </div>
+  <>
+    <div className={'btn-stack'}>
+      <Button color="primary">Filled Primary</Button>
+      <Button variant="outlined" color="primary">
+        Outlined Primary
+      </Button>
+      <Button variant="void" color="primary">
+        Void Primary
+      </Button>
+    </div>
+    <br />
+    <div className={'btn-stack'}>
+      <Button color="success">Filled Success</Button>
+      <Button variant="outlined" color="success">
+        Outlined Success
+      </Button>
+      <Button variant="void" color="success">
+        Void Success
+      </Button>
+    </div>
+    <br />
+    <div className={'btn-stack'}>
+      <Button color="warning">Filled Warning</Button>
+      <Button variant="outlined" color="warning">
+        Outlined Warning
+      </Button>
+      <Button variant="void" color="warning">
+        Void Warning
+      </Button>
+    </div>
+    <br />
+    <div className={'btn-stack'}>
+      <Button color="danger">Filled Danger</Button>
+      <Button variant="outlined" color="danger">
+        Outlined Danger
+      </Button>
+      <Button variant="void" color="danger">
+        Void Danger
+      </Button>
+    </div>
+  </>
 );
 
 export const Sizes = () => (
   <div className={'btn-stack'}>
+    <Button variant="filled" size="lg">
+      Large
+    </Button>
+    <Button variant="filled">Default</Button>
     <Button variant="filled" size="sm">
       Small
     </Button>
-    <Button variant="filled">Default</Button>
-    <Button variant="filled" size="lg">
-      Large
+  </div>
+);
+
+export const WithIcon = () => (
+  <div className={'btn-stack'}>
+    <Button startIcon disabled>
+      Primary
+    </Button>
+    <Button endIcon disabled>
+      Primary
     </Button>
   </div>
 );
